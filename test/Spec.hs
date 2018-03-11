9e4fc120d01f4cbd061268f4fd1e816043f05ec7
Thomas Letan <contact@thomasletan.fr>
2018-03-11 14:46:27 +0100

    test: Add proto-tests to check [Flux] operators are asynchronous

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,38 @@
+{-# LANGUAGE Arrows #-}
+
+module Main where
+
+import           Control.Arrow
+import           Control.Concurrent
+import           Control.Exception  (assert)
+import           Control.Monad      (when)
+import           Data.Flux
+import           Data.Time.Clock    (NominalDiffTime, diffUTCTime,
+                                     getCurrentTime)
+
+wait :: Int -> Producer ()
+wait n = Producer $ do
+  threadDelay (n * 1000000)
+  pure ([()], wait n)
+
+delay' :: Int -> Flux a a
+delay' n = Flux $ \x -> threadDelay (n * 1000000) >> pure (x, delay' n)
+
+chrono :: IO a
+       -> IO NominalDiffTime
+chrono x = do
+  before <- getCurrentTime
+  _ <- x
+  after <- getCurrentTime
+  pure (diffUTCTime after before)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  dt1 <- chrono $ once () (h <$> delay' 2 <*> delay' 2)
+  dt2 <- chrono $ once () (proc i -> do
+                              (x, y) <- (delay' 2 &&& delay' 2) -< i
+                              returnA -< h x y)
+
+  when (abs (dt2 - 2.0) > 0.1) (error "we have waited too long for the first test")
+  when (abs (dt1 - 2.0) > 0.1) (error "we have waited too long for the second test")
+  where h _ _ = ()
